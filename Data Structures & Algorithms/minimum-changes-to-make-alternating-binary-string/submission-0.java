76a300a205b6e94a31457b2cb0e60882060747bd
Samith-45 <samithsuryakolan@gmail.com>
2026-06-24 21:25:06 +0530

    Add: minimum-changes-to-make-alternating-binary-string - submission-0

---
+++ Data Structures & Algorithms/minimum-changes-to-make-alternating-binary-string/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minOperations(String s) {
+
+        int mismatchStart0 = 0;
+        int mismatchStart1 = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char expected0 = (i % 2 == 0) ? '0' : '1';
+            char expected1 = (i % 2 == 0) ? '1' : '0';
+
+            if (s.charAt(i) != expected0) {
+                mismatchStart0++;
+            }
+
+            if (s.charAt(i) != expected1) {
+                mismatchStart1++;
+            }
+        }
+
+        return Math.min(mismatchStart0, mismatchStart1);
+    }
+}
