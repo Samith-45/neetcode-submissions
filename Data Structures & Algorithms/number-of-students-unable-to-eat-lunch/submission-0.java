cb9aeae7b71e543bf860603570388e42ac5dcadd
Samith-45 <samithsuryakolan@gmail.com>
2026-06-30 10:58:03 +0530

    Add: number-of-students-unable-to-eat-lunch - submission-0

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int[] count = new int[2];
+        for (int s : students) {
+            count[s]++;
+        }
+
+        int i = 0;
+        while (i < sandwiches.length && count[sandwiches[i]] > 0) {
+            count[sandwiches[i]]--;
+            i++;
+        }
+
+        return sandwiches.length - i;
+    }
+}
